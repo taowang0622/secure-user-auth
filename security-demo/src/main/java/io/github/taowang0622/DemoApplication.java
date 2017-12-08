79e07881cc43a972500ea0d1a8d2f93310b6cb61
TaoWang <taowang0622@gmail.com>
2017-12-08 18:34:56 -0500

    project architecture finished

---
+++ security-demo/src/main/java/io/github/taowang0622/DemoApplication.java
@@ -0,0 +1,26 @@
+package io.github.taowang0622;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+@RestController
+public class DemoApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring Security!!";
+    }
+}
